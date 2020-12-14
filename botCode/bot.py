c2a04eaa979743cf22738c16fbc10ee03dc35c1e
Toby Dixon <thetjdixon1234@gmail.com>
2020-12-14 00:56:28 +0000

    added rmtarget and targets added to json file. Also removed ability to create and remove targets if you are targeted

--- botCode/bot.py
+++ botCode/bot.py
@@ -1,25 +1,70 @@
 import discord
 import json
+import random
+
+from discord.ext import commands
 
 with open("poultonpeeps.json") as json_file:
     pp = json.load(json_file)
 
 users = pp["Users"]
 TOKEN = pp["Token"]
+comebacks = pp["Comebacks"]
+targets = pp["Targets"]
 
-client = discord.Client()
+bot = commands.Bot('!')
 
-@client.event
+
+
+@bot.command()
+async def comeback(ctx, ):
+    if ctx.message.author.id not in targets:
+        index = random.randint(0, len(comebacks))
+        await ctx.send(comebacks[index])
+
+
+@bot.command()
+async def target(ctx, member: discord.Member = None):
+    if ctx.message.author.id not in targets:
+        if member == None:
+            await ctx.send("User not found")
+            return
+        targets.append(member.id)
+        pp["Targets"] = targets
+        with open("poultonpeeps.json", "w") as f:
+            json.dump(pp, f)
+
+@bot.command()
+async def rmtarget(ctx, member: discord.Member = None):
+    if ctx.message.author.id not in targets:
+        if member == None:
+            await ctx.send("User not found")
+            return
+        if member.id not in targets:
+            await ctx.send("User not targeted")
+            return
+        else:
+            targets.remove(member.id)
+            with open("poultonpeeps.json", "w") as f:
+                json.dump(pp, f)
+
+
+@bot.event
 async def on_message(message):
-    if message.author.id not in [users["Tommy"], users["Huw"]]:
+    if message.author.id in targets:
         await message.delete()
-    if message.author.id not in [users["Tommy"], users["Huw"]]:
-        await client.get_channel(777630408438448171).send("shut up", tts = False)
+    if message.author.id in targets:
+        channel = message.channel
+        await channel.send("shut up", tts = False)
+    await bot.process_commands(message)
 
-@client.event
+@bot.event
 async def on_voice_state_update(member, before, after):
-    if(member.id not in [users["Tommy"], users["Huw"]]):
+    if(member.id in targets):
         if(after.mute == False):
             await member.edit(mute=True)
 
-client.run(TOKEN)
+
+
+
+bot.run(TOKEN)
